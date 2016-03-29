c084f72ad95f3aa42e76d3132376f082ab976ed2
Katie Hanway <klwillzk@gmail.com>
2016-03-29 17:34:44 -0400

    Add note form.

---
+++ app/notes/notes.js
@@ -0,0 +1,27 @@
+(function() {
+  angular.module('notely.notes', [
+    'ui.router'
+  ])
+    .config(notesConfig);
+
+  notesConfig.$inject = ['$stateProvider'];
+  function notesConfig($stateProvider) {
+    $stateProvider
+
+      .state('notes', {
+        url: '/notes',
+        template: '<h1>Notely</h1><p>{{ message }}</p><div ui-view></div>',
+        //templateUrl: '/notes/notes.html',
+        controller: NotesController
+      })
+
+      .state('notes.form', {
+        url:'/:noteId',
+        templateUrl: '/notes/notes-form.html'
+      });
+  }
+NotesController.$inject = ['$scope'];
+  function NotesController($scope) {
+    $scope.message="I <3 Angular."
+  }
+})();
